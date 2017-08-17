04849dfd5b77518806e06934341ad0597f506628
hoidol <qkrghdud0@gmail.com>
2017-08-17 21:31:56 +0900

    Route _Data 완성

--- app/src/main/java/com/iguideu/data/Route_Data.java
+++ app/src/main/java/com/iguideu/data/Route_Data.java
@@ -21,36 +21,34 @@ public class Route_Data {
     public String User_ID;
     public String User_Name;
     public String User_Profile_URL;
+
+
     public String Route_Time_Of_Write;
-    public List<String> Route_Locations; //목적지만 표시
-    public List<String> Route_Total_Locations; // 목적지 + 경유지 표시
+    public String Route_Main_Title;
     public List<String> Route_Photo_URLs; // Route Detail에서 보여줄 사진들의 URL
-    public String Route_Title;
-    public String Route_Content;
+    public Boolean Route_Possibility;
     public String Route_Available_Time;
     public int Route_Required_Time;
     public int Route_Tourist_Num;
-    public int Route_Rating_Num;
-    public Boolean Route_Possibility;
+    public List<Route_Pin_Data> Route_Locations; //목적지만 표시
 
-    public Route_Data(String Route_Index, String User_ID,String User_Name, String User_Profile_URL, String Route_Time_Of_Write, List<String> Route_Locations, List<String> Route_Total_Locations, List<String> Route_Photo_URLs,
-                      String Route_Title, String Route_Content, String Route_Available_Time, int Route_Required_Time, int Route_Tourist_Num, int Route_Rating_Num, Boolean Route_Possibility){
+    public int Route_Rating_Num;
+
+    public Route_Data(String Route_Index, String User_ID, String User_Name, String User_Profile_URL,String Route_Time_Of_Write, String Route_Main_Title, List<String> Route_Photo_URLs,
+                      Boolean Route_Possibility,String Route_Available_Time,int Route_Required_Time, int Route_Tourist_Num, List<Route_Pin_Data> Route_Locations,int Route_Rating_Num
+                      ){
         this.Route_Index = Route_Index;
         this.User_ID = User_ID;
         this.User_Name = User_Name;
         this.User_Profile_URL = User_Profile_URL;
         this.Route_Time_Of_Write = Route_Time_Of_Write;
-        this.Route_Locations =Route_Locations;
-        this.Route_Total_Locations =Route_Total_Locations;
+        this.Route_Main_Title= Route_Main_Title;
         this.Route_Photo_URLs = Route_Photo_URLs;
-        this.Route_Title = Route_Title;
-        this.Route_Content = Route_Content;
+        this.Route_Possibility = Route_Possibility;
         this.Route_Available_Time = Route_Available_Time;
         this.Route_Required_Time = Route_Required_Time;
-        this.Route_Tourist_Num =Route_Tourist_Num;
+        this.Route_Tourist_Num = Route_Tourist_Num;
+        this.Route_Locations = Route_Locations;
         this.Route_Rating_Num = Route_Rating_Num;
-        this.Route_Possibility = Route_Possibility;
     }
-
-
 }
---
+++ app/src/main/java/com/iguideu/data/Route_Pin_Data.java
@@ -0,0 +1,21 @@
+package com.iguideu.data;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by Hoyoung on 2017-08-17.
+ */
+@IgnoreExtraProperties
+public class Route_Pin_Data {
+
+    public String Route_Title;
+    public String Route_Content;
+    public LatLng Route_Pin_Point;
+
+    public Route_Pin_Data(String Route_Title,String Route_Content ,LatLng Route_Pin_Point){
+        this.Route_Title = Route_Title;
+        this.Route_Content = Route_Content;
+        this.Route_Pin_Point = Route_Pin_Point;
+    }
+}
